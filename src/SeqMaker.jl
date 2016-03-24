d3bfba03bec414582967e475cbf86703514798b6
Shifu Chen <chen@haplox.com>
2016-03-24 13:14:01 +0800

    usine Pkg.dir

--- src/SeqMaker.jl
+++ src/SeqMaker.jl
@@ -17,7 +17,9 @@ include("maker.jl")
 # test
 export seqtest
 function seqtest(outdir="output")
-    make_seq("data/panels/lung_cancer_hg19.bed", "data/profiles/fusion.json", outdir)
+    panel_file = joinpath(Pkg.dir("SeqMaker"), "data/panels/lung_cancer_hg19.bed")
+    profile_file = joinpath(Pkg.dir("SeqMaker"), "data/profiles/fusion.json")
+    make_seq(panel_file, profile_file, outdir)
 end
 
 end # module
