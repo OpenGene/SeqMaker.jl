1e99bafb846f8307e824e2d573691daa9b68a038
Shifu Chen <chen@haplox.com>
2016-03-25 09:23:54 +0800

    use ngs() interface

--- README.md
+++ README.md
@@ -14,6 +14,10 @@ Pkg.clone("https://github.com/sfchen/SeqMaker.jl.git")
 
 using SeqMaker
 
-# it will download human genome assembly data automatically
-seqtest("foldername")
+# it will download human genome assembly data automatically, please make sure your system can access internet
+# ngs(outdir, panel_file, profile_file)
+# outdir is default to "output"
+# panel_file is default to "data/panels/lung_cancer_hg19.bed"
+# profile is default to "data/profiles/example.json"
+ngs()
 ```
--- src/SeqMaker.jl
+++ src/SeqMaker.jl
@@ -15,10 +15,14 @@ include("sequencing.jl")
 include("maker.jl")
 
 # test
-export seqtest
-function seqtest(outdir="output")
-    panel_file = joinpath(Pkg.dir("SeqMaker"), "data/panels/lung_cancer_hg19.bed")
-    profile_file = joinpath(Pkg.dir("SeqMaker"), "data/profiles/fusion.json")
+export ngs
+function ngs(outdir="output", panel_file="", profile_file="")
+    if panel_file==""
+        panel_file = joinpath(Pkg.dir("SeqMaker"), "data/panels/lung_cancer_hg19.bed")
+    end
+    if profile_file==""
+        profile_file = joinpath(Pkg.dir("SeqMaker"), "data/profiles/example.json")
+    end
     make_seq(panel_file, profile_file, outdir)
 end
 
