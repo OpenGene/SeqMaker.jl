f82c581a6cf9cedb87a2e2e135e3098a2aad6fe0
Shifu Chen <chen@haplox.com>
2016-06-19 22:37:25 +0800

    add test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,12 @@
 using SeqMaker
+using OpenGene
 using Base.Test
 
+function test_ngs()
+    ngs(depth = 0.01)
+    return true
+end
+
 # write your own tests here
-@test 1 == 1
+info("Test starts, this may take minutes to hours if it need to download the genome assembly")
+@test test_ngs()
